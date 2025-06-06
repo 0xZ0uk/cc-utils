9c4e8c08ba0aff39242a33a2de59e101727f2dc5
Pedro Santana <pedrsntana@proton.me>
2025-06-06 10:30:23 +0100

    Update installer

--- installer.lua
+++ installer.lua
@@ -1,4 +1,4 @@
-local baseUrl = "https://raw.githubusercontent.com/yourusername/yourrepo/main/"
+local baseUrl = "https://raw.githubusercontent.com/0xZ0uk/cc-utils/main/"
 
 local files = {
     -- General utils
